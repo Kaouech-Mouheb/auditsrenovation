b3f2b8ff153aeaad3550d49a1890a1424cee1e37
kaouech mouheb <mouhebkaouach@hotmail.fr>
2022-02-16 18:07:32 +0100

    kdj

--- dist/_nuxt/static/1645029830/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fauditsrenovationstatic\u002F_nuxt\u002Fstatic\u002F1645029830",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fauditsrenovationstatic\u002F",assetsPath:"\u002Fauditsrenovationstatic\u002F_nuxt\u002F",cdnURL:a}}}}(null));

b5d97efb0156cae1dbb2727590c21419ccc05d1a
kaouech mouheb <mouhebkaouach@hotmail.fr>
2022-02-16 15:01:48 +0100

    site audits renovation premiére version

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <v-app-bar fixed app>
+      <v-toolbar-title>
+        <v-img src="/auditsrenovationlogo.png" class="logo"></v-img>
+      </v-toolbar-title>
+
+      <v-spacer />
+      <div>
+        <v-btn disabled text x-large class="small-screen">
+          <v-icon medium class="mr-2"> mdi-phone-forward-outline </v-icon>
+          Contactez-nous</v-btn
+        >
+
+        <v-btn color="primary" text x-large class="small-screen">
+          06 18 66 29 19
+        </v-btn>
+        <v-btn color="primary" class="small-screen-active" text small>
+          <v-icon small class="mr-2e"> mdi-phone-forward-outline </v-icon>
+          06 18 66 29 19
+        </v-btn>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      title: "audits rénovation",
+    };
+  },
+};
+</script>
+<style scoped>
+.theme--light.v-app-bar.v-toolbar.v-sheet {
+  background-color: #fffdfd;
+}
+.theme--light.v-footer {
+  background-color: #ffffff;
+  color: rgba(0, 0, 0, 0.87);
+}
+.small-screen-active {
+  display: none;
+}
+.logo {
+  width: 250px;
+}
+@media (max-width: 575.98px) {
+  .small-screen {
+    display: none;
+  }
+  .small-screen-active {
+    display: block;
+  }
+  .logo {
+    width: 150px;
+  }
+}
+@media (min-width: 576px) and (max-width: 991.98px) {
+  .logo{
+    width: 170px;
+  }
+ }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,564 @@
+<template>
+  <div class="rating">
+    <v-row class="bg-img" justify="center">
+      <div class="block-value"></div>
+      <v-col cols="12" md="6" sm="12" class="margin-bg">
+        <h2 class="mt-4">
+          Vous avez un projet <strong>d'isolation themrique </strong> ou de
+          <strong>rénovation</strong> ? Vous envisagez actuellement de
+          <strong>rénover</strong> votre bien immobilier, mais vous n’avez pas
+          d’idées concernant les moyens pour les réaliser.
+        </h2>
+
+        <p class="mt-4">
+          Pour tous vos <strong>travaux d’isolation thermique</strong>,
+          <strong
+            ><span class="d-none">travaux</span> d’aménagement de
+            combles</strong
+          >,
+          <strong> <span class="d-none">travaux</span> de couverture</strong>,
+          <strong><span class="d-none">travaux</span> de façades </strong> et de
+          <strong>rénovation</strong> .<strong>AUDITS rénovation</strong> mais à
+          votre disposition Une équipe dédiée.
+        </p>
+        <p>
+          Vous pouvez nous contactez par téléphone ou bien remplir notre
+          formulaire en ligne. 100% de clients satisfaits ! Devis Gratuit sans
+          engagement.
+        </p>
+        <p>
+          Sur simple demande, bénéficiez dès maintenant de notre bilan thermique
+          à domicile, entièrement gratuit et sans engagement, afin d’établir un
+          diagnostic précis de l’état de l’isolation de votre maison.
+        </p>
+      </v-col>
+      <v-col cols="12" md="4" sm="12" class="mt-4">
+        <v-card>
+          <v-card-title class="text-primary"> Devis Gratuit </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" md="8">
+                Remplissez ce formulaire pour recevoir votre devis gratuit et
+                sans engagement sous 24h ouvrés.
+              </v-col>
+              <v-col cols="12" md="4">
+                <img src="/rge.jpeg" width="70px" />
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-card-text>
+            <v-form>
+              <v-select
+                :items="[
+                  'Isolation des combles',
+                  'Aménagement de combles',
+                  'Traitement Bois Charpente',
+
+                  'isolation thermique',
+                  'travaux de couverture',
+                  'travaux de peinture',
+                  'Isolation des murs par l\'intérieur',
+                  'Isolation des murs par l\'extérieur',
+                  'Ravalement de façade',
+                  'Traitement de façade',
+                  'Isolation toiture par l\’extérieur',
+                  'Isolation sous toiture',
+                  'Rénovation de toiture',
+                  'Démoussage toiture',
+                  'Traitement de toiture',
+                  'Menuiseries, fenêtres, portes',
+                  'Ventilation, VMC',
+                  'Isolation garage',
+                  'Autres',
+                ]"
+                label="Type de travaux"
+                required
+              ></v-select>
+
+              <v-text-field label="Nom *" single-line></v-text-field>
+              <v-text-field label="code postal *" single-line></v-text-field>
+              <v-text-field
+                label="votre téléphone *"
+                single-line
+              ></v-text-field>
+              <v-text-field label="votre email *" single-line></v-text-field>
+              <v-textarea
+                clearable
+                clear-icon="mdi-close-circle"
+                label="Décrivez votre projet et leur de rappel souhaitée"
+              ></v-textarea>
+              <v-btn color="primary">Envoyer ma demande de devis</v-btn>
+              <small class="d-block mt-4">
+                Vos données personnelles sont collectées exclusivement dans le
+                cadre de votre demande de devis et ne font l’objet d’aucune
+                diffusion ni commercialisation. Elles sont destinées au
+                personnel de Mieux Rénover en charge du traitement de votre
+                demande. Le présent site est déclaré à la CNIL sous le n°
+                1502515v0.
+              </small>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="m-4">
+      <v-card width="100%">
+        <v-row justify="center">
+          <v-col cols="12" md="2">
+            <img src="/decennale.png" class="logo-decennal" alt="" />
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-card-title class="text-center text-primary">
+              Responsabilité civile professionnelle
+            </v-card-title>
+            <v-card-text>
+              L’entreprise AUDITS Rénovation couvre et garantit l’intégralité
+              des dommages qui apparaissent après la réception des travaux
+              pendant 10 ans.
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-row>
+
+    <v-row class="m-4 small-screen">
+      <v-card>
+        <v-row justify="center">
+          <v-col cols="12" md="5">
+            <span class="h4 mt-4 d-block p-2 text-center text-primary"
+              >Découvrez nos experts par métier du bâtiment</span
+            >
+            <hr />
+          </v-col>
+        </v-row>
+        <v-row class="m-2" justify="center">
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="plaquiste.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Plaquiste</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="macon.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary">Maçon</v-card-title>
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="electricien.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Électricien</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="plombier.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Plombier</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row class="m-2" justify="center">
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="menuisier.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Menuisier</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="peintre.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Peintre</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="carreleur.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Carreleur</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-card>
+              <v-img
+                src="isolation1.jpg"
+                width="100%"
+                height="300px"
+                alt=""
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              >
+                <v-card-title class="text-light bg-primary"
+                  >Isolation</v-card-title
+                >
+              </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-row>
+
+    <v-row class="m-4">
+      <v-card width="100%">
+        <v-row justify="center">
+          <v-col cols="12" md="3">
+            <div class="h2 text-center">
+              <span class="text-primary"> Nos </span> points fort
+            </div>
+            <hr />
+          </v-col>
+        </v-row>
+        <v-row class="m-2">
+          <div class="rapport-prix">
+            <img src="/rapportqualite.png" class="img-rapport-prix" alt="" />
+          </div>
+          <v-col cols="12" md="3">
+            <div class="d-flex flex-column align-items-center">
+              <div>
+                <v-icon color="primary" x-large
+                  >mdi-truck-delivery-outline</v-icon
+                >
+              </div>
+              <div class="h5 text-primary">Livraison rapide</div>
+            </div>
+          </v-col>
+          <v-col cols="12" md="3">
+            <div class="d-flex flex-column align-items-center">
+              <div>
+                <v-icon color="primary" x-large
+                  >mdi-calculator-variant-outline</v-icon
+                >
+              </div>
+              <div class="h5 text-primary">Devis Gratuit</div>
+            </div>
+          </v-col>
+          <v-col cols="12" md="3">
+            <div class="d-flex flex-column align-items-center">
+              <div>
+                <v-icon color="primary" x-large
+                  >mdi-sun-thermometer-outline</v-icon
+                >
+              </div>
+              <div class="h5 text-primary">Coeficient thermique Excelent</div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-row>
+    <v-row class="small-screen" justify="center">
+      <v-col cols="12" md="5">
+        <v-carousel>
+          <v-carousel-item
+            v-for="(item, i) in imagesRenovation"
+            :key="i"
+            :src="item"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="text-h2">Rénovation</div>
+            </v-row>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+      <v-col cols="12" md="5">
+        <v-carousel>
+          <v-carousel-item
+            v-for="(item, i) in imagesRavalement"
+            :key="i"
+            :src="item"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="text-h2">Isolation</div>
+            </v-row>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+    <v-row>
+      <hr />
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" md="3" sm="12">
+        <v-card height="200px">
+          <v-card-title> Adresse </v-card-title>
+          <v-card-text>
+            <address>
+              Adresse: 3 abenue de 25 aout 1944 <br />
+              Choisy Le Roi
+            </address>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3" sm="12">
+        <v-card height="200px">
+          <v-card-title> Coordonnées de contact </v-card-title>
+          <v-card-text>
+            Email: auditsrenovation@gmail.com <br />
+            téléphone : 06 18 66 29 19
+            <v-btn color="primary">demander un devis</v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="3" sm="12" class="mt-2">
+        <v-card height="200px" color="blue darken-3" class="text-light">
+          <v-row>
+            <v-col>
+              <h3 class="m-4">
+                Avis client
+                <span class="grey--text text--lighten-2 text-caption mr-2">
+                  ({{ 5 }}/5) basé sur 14 avis
+                </span>
+              </h3>
+              <div class="m-4">
+                <span v-for="i in 5" :key="i">
+                  <v-icon color="orange"> mdi-star </v-icon>
+                </span>
+              </div>
+              <v-divider class="mx-4" color="white"></v-divider>
+              <small><a href="" class="text-light m-4">voir les avis</a></small>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <hr />
+    </v-row>
+    <div>
+      <v-footer>
+        <v-row>
+          <v-col cols="12" md="4">
+            <h3 class="h3 m-4">Isolation thermique</h3>
+            <v-divider class="mx-4" color="white"></v-divider>
+            <ul v-for="i in itemsIsolation" :key="i">
+              <li class="list-unstyled">{{ i }}</li>
+            </ul>
+          </v-col>
+          <v-col cols="12" md="4">
+            <h3 class="h3 m-4">Toiture</h3>
+            <v-divider class="mx-4" color="white"></v-divider>
+            <ul v-for="i in itemsToiture" :key="i">
+              <li class="list-unstyled">{{ i }}</li>
+            </ul>
+          </v-col>
+          <v-col cols="12" md="4">
+            <h3 class="h3 m-4">Rénovation</h3>
+            <v-divider class="mx-4" color="white"></v-divider>
+            <ul v-for="i in itemsRenovation" :key="i">
+              <li class="list-unstyled">{{ i }}</li>
+            </ul>
+          </v-col>
+          <v-divider class="mx-4" color="white"></v-divider>
+          <v-col class="text-center" cols="12">
+            {{ new Date().getFullYear() }} — <strong>AUDITS Rénovation</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data: () => ({
+    itemsIsolation: [
+      "— Aménagement de combles",
+      "— Isolation comble perdu",
+      "— Isolation des combles aménagés par l’extérieur",
+      "— Isolation des combles aménagés par l’intérieur",
+      "— Isolation thermique par l’extérieur",
+      "— Isolation thermique par l’intérieur",
+    ],
+    itemsToiture: [
+      "— Traitement de charpentes",
+      "— Remaniement et rénovation de toiture",
+      "— Hydrofuge de toiture",
+      "— Les gouttières et descentes",
+      "— Velux",
+    ],
+    itemsRenovation: [
+      "— Travaux de peinture",
+      "— Menuiseries, fenêtres, portes",
+      "— Carrelage, parquets, revêtements de sol",
+      "— Ventilation, VMC",
+    ],
+    imagesRenovation: [
+      "bureau.jpg",
+      "bain.jpg",
+      "cuisine.jpg",
+      "bainmarbre.jpg",
+    ],
+    imagesRavalement: [
+      "ite.jpg",
+      "ravalement.jpg",
+      "itecomble.jpg",
+      "couverture.jpg",
+    ],
+  }),
+};
+</script>
+<style scoped>
+.rating {
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+}
+.bg-img {
+  color: rgb(252, 252, 252);
+  font-size: 20px;
+  background-image: url(~/static/isolation.jpg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  height: 900px;
+}
+.rapport-prix {
+  margin-top: -70px;
+}
+
+.block-value {
+  position: absolute;
+  width: 100%;
+  height: 900px;
+  background: #0095f1;
+  background: linear-gradient(
+    90deg,
+    rgba(0, 149, 241, 0.7) 0,
+    rgba(41, 184, 248, 0.7)
+  );
+}
+.margin-bg {
+  margin-top: 150px;
+}
+.logo-decennal {
+  width: 200px;
+}
+
+
+@media (max-width: 575.98px) {
+  .margin-bg[data-v-2a183b29] {
+    margin-top: 40px;
+  }
+  h2,
+  .h2,
+  .bg-img[data-v-2a183b29] {
+    font-size: 1.3rem;
+    margin-bottom: 0.5rem;
+    font-weight: 300;
+  }
+
+  .bg-img {
+    height: 1870px;
+  }
+  .block-value {
+    height: 1870px;
+  }
+  .logo-decennal {
+    width: 140px;
+    display: block;
+    margin: 0 auto 0 auto;
+  }
+  .text-primary {
+    font-size: 1rem;
+    font-weight: 370;
+  }
+  .img-rapport-prix {
+    width: 110px;
+  }
+  .rapport-prix {
+    margin: 10px auto 10px auto;
+  }
+  .m-4 {
+    margin-left: -10px;
+    margin-right: -10px;
+  }
+  .small-screen {
+    display: none;
+  }
+}
+@media (min-width: 576px) and (max-width: 991.98px) {
+ .bg-img {
+    height: 1600px;
+  }
+  .block-value {
+    height: 1600px;
+  }
+   .img-rapport-prix {
+    width: 140px;
+  }
+  .rapport-prix {
+    margin: 10px auto 10px auto;
+  }
+   .logo-decennal {
+
+    display: block;
+    margin: 0 auto 0 auto;
+  }
+ }
+</style>
